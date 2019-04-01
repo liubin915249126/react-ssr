941f08eae151b85a5a7e67c6174be253ac577ea0
liubin915249126 <915249126@qq.com>
2019-04-01 10:43:12 +0800

    init

--- pages/index1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Index1 extends React.Component {
-    render(){
-        return(
-            <div>1lknjmlkdffdjn111</div>
-        )
-    }
-}
--- pages/index2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Index2 extends React.Component {
-    render(){
-        return(
-            <div>2222222222</div>
-        )
-    }
-}
