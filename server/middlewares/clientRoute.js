5de375be10ffacb17c2c15ddfd97023c59f21510
liubin915249126 <915249126@qq.com>
2019-04-01 15:53:43 +0800

    add orderType

--- server/middlewares/clientRoute.js
+++ server/middlewares/clientRoute.js
@@ -1,31 +1,54 @@
-import React from 'react'
-import {renderToString} from 'react-dom/server'
-import {match, RouterContext} from 'react-router'
-import {Provider} from 'react-redux'
-import routes from '../../client/routes'
-import configureStore from '../../client/common/store/configureStore'
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { match, RouterContext } from "react-router";
+import { Provider } from "react-redux";
+import routes from "../../client/routes";
+import configureStore from "../../client/common/store/configureStore";
 
-const store = configureStore()
+import dva, { connect } from "dva";
+import { browserHistory } from 'dva/router';
+
+const app = dva({
+    history: browserHistory
+    // history: createHistory()
+});
+
+// 2. Plugins
+// app.use({});
+
+// 3. Model move to router
+models.forEach(m => {
+    app.model(m);
+});
+
+// 4. Router
+app.router(require("./client/routes"));
+
+// 5. Start
+app.start('#root');
+
+const store = configureStore();
 
 async function clientRoute(ctx, next) {
-    let _renderProps
+    let _renderProps;
 
-    match({routes, location: ctx.url}, (error, redirectLocation, renderProps) => {
-        _renderProps = renderProps
-    })
+    match(
+        { routes, location: ctx.url },
+        (error, redirectLocation, renderProps) => {
+            _renderProps = renderProps;
+        }
+    );
 
     if (_renderProps) {
-        await ctx.render('index', {
+        await ctx.render("index", {
             root: renderToString(
-                <Provider store={store}>
-                    <RouterContext {..._renderProps}/>
-                </Provider>
+                app
             ),
             state: store.getState()
-        })
+        });
     } else {
-        await next()
+        await next();
     }
 }
 
-export default clientRoute
+export default clientRoute;
